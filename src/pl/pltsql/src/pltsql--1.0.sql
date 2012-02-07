81a5721383d6786290d6d2708151f7606856c592
Affan Salman <affan.salman@gmail.com>
2012-02-07 18:43:46 +0000

    Make the EXTENSION support for pltsql functional.
    INSERT into pg_catalog.pg_pltemplate as part of the EXTENSION SQL file
    instead of making invasive bootstrap catalog entries in the core.  By
    doing so, keep pltsql flexible so it can be installed simply with a
    CREATE EXTENSION invocation as a self-contained package.
    
    This does mean, however, that the EXTENSION will require superuser
    privilege to install it but the control metadata makes that explicit for
    the users at least as far as the corresponding error is concerned.
    
    (To support "CREATE LANGUAGE pltsql" which the EXTENSION uses under the
    covers, we need to either manually CREATE the corresponding language
    support functions (call handler, validator, ...) and use the
    corresponding long CREATE LANGUAGE form OR make an entry into
    pg_pltemplate to be able to skip the support-function clauses while
    creating the LANGUAGE.
    
    Both require superuser privileges: language handler functions are
    UNTRUSTED (C functions) and pg_pltemplate is a system catalog.)

--- src/pl/pltsql/src/pltsql--1.0.sql
+++ src/pl/pltsql/src/pltsql--1.0.sql
@@ -1,6 +1,26 @@
 /* src/pl/pltsql/src/pltsql--1.0.sql */
 
 /*
+ * Cluster-wide objects are not tracked as EXTENSION members.  As pg_pltemplate
+ * is a shared catalog, we need to make the pltsql entry in a resilient manner.
+ *
+ * We do not want a "CREATE EXTENSION" invocation to error out if the pltemplate
+ * catalog entry already exists e.g. when the user installs the language in a
+ * different database under the same cluster.  The user could always CREATE the
+ * LANGUAGE directly but that is not recommended.
+ *
+ * We INSERT the row only if it does not already exist.  We use (SELECT 1 ...)
+ * in the subquery as we have no interest in the pltsql record (unique) but the
+ * existence status thereof.
+ */
+INSERT INTO pg_catalog.pg_pltemplate (tmplname, tmpltrusted, tmpldbacreate,
+            tmplhandler, tmplinline, tmplvalidator, tmpllibrary)
+SELECT      'pltsql', true, true, 'pltsql_call_handler',
+            'pltsql_inline_handler', 'pltsql_validator', '$libdir/pltsql'
+WHERE NOT EXISTS (
+    SELECT 1 FROM pg_catalog.pg_pltemplate WHERE tmplname = 'pltsql');
+
+/*
  * Currently, all the interesting stuff is done by CREATE LANGUAGE.
  * Later we will probably "dumb down" that command and put more of the
  * knowledge into this script.
